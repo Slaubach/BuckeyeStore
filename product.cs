e4a221269ce83468df6b252736917db490ef5221
Slaubach <launch.17@osu.edu>
2017-04-11 13:24:25 -0400

    lab 9

---
+++ product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BuckeyeStore
+{
+    public class product
+
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string brand { get; set; }
+        public review review { get; set; }
+        public AggregateException aggregateRating { get; set; }
+        public list<offer> offers { get; set; }
+        public string sku { get; set; }
+
+    }
+
+    public class list<T>
+    {
+    }
+
+    public class offer
+    {
+    }
+
+    public class review
+    {
+        public int id { get; set; }
+        public string body { get; set; }
+        public Rating rating { get; set; }
+
+    }
+
+    public class Rating
+    {
+    }
+
+    public void addreview(review review)
+    {
+        this.Review = review;
+    }
+}
---
+++ producttest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuckeyeStore
+{
+    class producttest
+    {
+        public void Test();
+        {
+        var product = new product
+        {
+            id = 1,
+            name = "t-shirt",
+            brand = "nike"
+        };
+        var review = new review
+        {
+            id = 1,
+            body = "a durable buckeye t-shirt"
+        };
+        product.addreview(review);
+            assert.notnull(product.review);
+            Assert.equal(review.id, priduct.review.id);
+            assert.exual(review.body, product.review.body);
+        }
+        }
+
+    
+
